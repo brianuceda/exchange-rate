9ac3f75e98d63c2972bdf141d358653bef80a32b
Brian Uceda <u202114785@upc.edu.pe>
2025-04-24 11:09:07 -0500

    feat(dev): test peru timezone

--- api/app.py
+++ api/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, jsonify
 import redis
 import os
-# import requests
-# from bs4 import BeautifulSoup
-# import datetime
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import pytz
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configuración de zona horaria para Perú
+peru_timezone = pytz.timezone('America/Lima')
 
 app = Flask(__name__)
 
@@ -16,13 +23,20 @@ redis_client = redis.Redis(
     decode_responses=True
 )
 
-@app.route('/test')
-def home():
+@app.route('/today')
+def exchange_rate_controller():
     try:
         redis_client.ping()
-        return jsonify(message="Redis connected successfully"), 200
+        return jsonify(message="Redis connected successfully - " + str(get_peru_datetime())), 200
     except Exception as e:
         return jsonify(message=str(e)), 500
 
+def exchange_rate_service():
+    return jsonify(message="Exchange rate service")
+
+# Función de utilidad para obtener la fecha y hora actual en zona horaria de Perú
+def get_peru_datetime():
+    return datetime.datetime.now(peru_timezone)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
